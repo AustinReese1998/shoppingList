a8d043cc2b37ca25188927f3af23fb2e32f6a061
AustinReese1998 <reesau01@luther.edu>
2018-02-28 13:45:16 -0600

    Javascript is operational

--- doStuff.js
+++ doStuff.js
@@ -1,9 +1,40 @@
 function addItem(){
-	let itemName = document.querySelector("#itemName")
+	let rawItem = document.getElementById("selItem")
+	let itemName = rawItem.options[rawItem.selectedIndex].text
+	let rawQuan = document.getElementById("selQuan")
+	let quanName = rawQuan.options[rawQuan.selectedIndex].text
+	let rawPrio = document.getElementById("selPrio")
+	let prioName = rawPrio.options[rawPrio.selectedIndex].text
+	let rawStor = document.getElementById("selStor")
+	let storName = rawStor.options[rawStor.selectedIndex].text
+	let rawCate = document.getElementById("selCate")
+	let cateName = rawCate.options[rawCate.selectedIndex].text
 	let myTable = document.querySelector("#nextItem")
+
 	let tr = document.createElement("tr")
-	let td = document.createElement("td")
-	td.innerHTML = itemName
-	tr.appendChild(td)
+
+	let itemCol = document.createElement("td")
+	let quanCol = document.createElement("td")
+	let prioCol = document.createElement("td")
+	let storCol = document.createElement("td")
+	let cateCol = document.createElement("td")
+	let pricCol = document.createElement("td")
+
+
+	itemCol.innerHTML = itemName
+	quanCol.innerHTML = quanName
+	prioCol.innerHTML = prioName
+	storCol.innerHTML = storName
+	cateCol.innerHTML = cateName
+	pricCol.innerHTML = "Not Implemented"
+
+
+	tr.appendChild(itemCol)
+	tr.appendChild(quanCol)
+	tr.appendChild(prioCol)
+	tr.appendChild(storCol)
+	tr.appendChild(cateCol)
+	tr.appendChild(pricCol)
+
 	myTable.appendChild(tr)
 }
