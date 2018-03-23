9bed512ae163aad82ea737654c2f06126c8b4972
AustinReese1998 <reesau01@luther.edu>
2018-03-22 19:51:55 -0500

    removed cors

--- storage.py
+++ storage.py
@@ -1,10 +1,7 @@
 from flask import Flask, Response, render_template, request, jsonify
 import random, json
-from flask_cors import CORS
 
 app = Flask(__name__)
-
-CORS(app)
 
 @app.route('/')
 def static_page():
